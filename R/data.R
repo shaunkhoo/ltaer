4e4223ef480ca43b3f540d90bc5a90912f65ef83
Shaun Khoo <shaunkhoo@Shauns-MacBook-Pro.local>
2018-12-10 23:34:34 -0500

    update 2

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Bus arrival timings
+#'
+#' This is a small dataset containing bus arrival timings for 11 randomly selected bus stops in Singapore.
+#'
+#' @format A data frame with 103 rows and 30 variables
+#' @source Called from the Bus Timings API on 11 December 2018 at 12.06pm
+"bus_arrivals"
