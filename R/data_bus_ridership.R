db8691509f5a11240906cd04e0b877a7c8df0200
Shaun Khoo <shaunkhoo@Shauns-MacBook-Pro.local>
2018-12-11 21:22:26 -0500

    add bus ridership data

---
+++ R/data_bus_ridership.R
@@ -0,0 +1,7 @@
+#' Bus ridership
+#'
+#' This is a dataset containing the total number of taps in and out for each bus stop in Singapore.
+#'
+#' @format A data frame with 4734 rows and 6 variables
+#' @source Downloaded from the Passenger Volume by Bus Stop API, and joined with the Bus Stops API
+"bus_ridership_cleaned"
