b42357c6a1de68a1ea522feab489a82fc9389494
Shaun Khoo <shaunkhoo@Shauns-MacBook-Pro.local>
2018-12-11 20:31:08 -0500

    amended traffic images for number of entries returned

--- R/getTrafficImages.R
+++ R/getTrafficImages.R
@@ -30,6 +30,29 @@ getTrafficImages <- function(api_key) {
     output <- rbind(output, t(unlist(interim)))
   }
   names(output) <- names(unlist(trafficimage_list[[1]]))
+
+  num <- 2
+  while(length(trafficimage_list) > 69) {
+    result <- httr::GET(paste("http://datamall2.mytransport.sg/ltaodataservice/Traffic-Images", "?$skip=", (num-1)*70, sep=""),
+                        httr::add_headers(AccountKey = api_key)
+    )
+    if (httr::http_status(result)$category != "Success") {
+      message("Unsuccessful API call for traffic images. Please check your parameters or connection.")
+      return(result)
+    } else {
+      message("Successful API call for traffic images. Processing page ", num, "...")
+    }
+    output0 <- data.frame(matrix(nrow = 0, ncol = 12))
+    trafficimage_list <- httr::content(result)[[2]]
+    for (i in 1:length(trafficimage_list)) {
+      interim <- trafficimage_list[[i]]
+      interim[sapply(interim, is.null)] <- 0
+      output0 <- rbind(output0, t(unlist(interim)))
+    }
+    output <- rbind(output, output0)
+    num <- num + 1
+  }
+
   output$CameraID <- as.character(output$CameraID)
   output$Latitude <- as.numeric(as.character(output$Latitude))
   output$Longitude <- as.numeric(as.character(output$Longitude))
